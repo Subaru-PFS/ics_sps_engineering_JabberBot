c42458e9c7bc271f463648cf46f4cf4db6ceba5d
LE FUR Arnaud <arnaud.lefur@lam.fr>
2016-03-16 14:25:10 +0000

    name updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys, os
 
 absolute_path = os.getcwd() + '/' + sys.argv[0].split('main.py')[0]
-sys.path.insert(0, absolute_path.split('Engineering_JabberBot')[0])
+sys.path.insert(0, absolute_path.split('ics_sps_engineering_JabberBot')[0])
 addr = sys.argv[1] if len(sys.argv) > 1 else "localhost"
 port = sys.argv[2] if len(sys.argv) > 2 else 5432
 from broadcast import JabberBotManager
